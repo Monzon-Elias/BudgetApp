5c0d6d1b5ca8d70b796e548e65ec16a212697ba7
eliasenchiletdamas@gmail.com <https://bitbucket.org/EliosMoncho/repo360/src/master/>
2025-10-01 21:06:45 -0600

    Remove Railway backend, use Netlify Functions only

--- config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    mongodbUri: 'mongodb+srv://elios:eliosmbaPass1!!@budgetapp.d6ntesg.mongodb.net/budgetAppDb?retryWrites=true&w=majority',
-    jwtSecret: 'budgetApp2025SecretKey!SuperSecure#MonzonProject',
-    port: 3000,
-    frontendUrl: process.env.NODE_ENV === 'production' 
-        ? 'https://budgetsite.netlify.app' 
-        : 'http://localhost:5500'
-};
-
